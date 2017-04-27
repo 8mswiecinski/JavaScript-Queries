df54978611e1a80d25f7a0fb3d8561074e110c2b
Frussel Brout Sarts <8mswiecinski@stpatrick.org>
2017-04-27 15:23:56 -0500

    finished up the project

--- javasscriptquery.js
+++ javasscriptquery.js
@@ -4,7 +4,21 @@ function samecolor(){
 
     let divs = doc.queryselectorAll("#samecolor div");
     for(let d=0; d<divs.length;d++){
-        divs[d].style.backgroundcolor = color;
-        divs[d].style.opacity += 0.25*d;
+              divs[d].style.opacity += 0.25*d;
     }
+}
+
+
+function diffcolor(){
+  const doc = document;
+  let yellow = "#aaaa00";
+  let blue = "#00aaaa";
+
+  let diffdivs = doc.querySelectorAll("#diffcolor div");
+  for(let d=0; d<diffdivs.length;d=d+2){
+    diffdivs[d].style.backgroundColor = yellow;
+  }
+  for(let d=1; d<diffdivs.length;d=d+2){
+    diffdivs[d].style.backgroundColor = blue;
+  }
 }
